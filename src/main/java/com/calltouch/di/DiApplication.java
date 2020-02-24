106dc25c6d9a1eb3dc0c7a457da759d8f91d5ab9
Denis Goryachev <dgoryachev@calltouch.net>
2020-02-24 19:58:16 +0300

    Stydy spring bean lifecycle

--- src/main/java/com/calltouch/di/DiApplication.java
+++ src/main/java/com/calltouch/di/DiApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Main app before");
 		SpringApplication.run(DiApplication.class, args);
+		System.out.println("Main app after");
 	}
 
 }
